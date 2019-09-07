43696be9ae7ba7cb40f60a3eda4404d4dbb750e4
KevineMusoni <kevinenicky@gmail.com>
2019-09-06 23:31:21 -0800

    cng

--- mypace/settings.py
+++ mypace/settings.py
@@ -5,6 +5,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
+# settings
 
 import os
 import django_heroku
